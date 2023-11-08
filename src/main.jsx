6260be338ca582d03b17f81decac689ba2299128
Doniyor2709 <xabibullayev68026@gmail.com>
2023-11-08 11:09:09 +0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import AuthContextProvider from "./context/isAuthContext.jsx";
 import StoreProvider from "./redux/store/index.jsx";
+import { ConfigProvider } from "antd";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
       <StoreProvider>
+        <ConfigProvider theme={{
+          token:{
+            colorPrimary:"orange"
+          }
+        }}>
+
         <App />
+        </ConfigProvider>
       </StoreProvider>
     </AuthContextProvider>
   </React.StrictMode>
